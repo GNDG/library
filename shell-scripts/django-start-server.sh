1a039e197a914c1d594f01231eda97c25fa5b1bc
Mannu Gupta <abhimanyu98986@gmail.com>
2017-09-26 17:41:38 +0530

    added django-server shell script

---
+++ shell-scripts/django-start-server.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# change directory to the project location
+cd /media/abhi/300Gb/Office/
+source venv/bin/activate
+# open with sublime, can be used other editors like atom
+subl itoucan_2017
+cd itoucan_2017
+python manage.py runserver
